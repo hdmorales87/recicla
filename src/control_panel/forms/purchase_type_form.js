30fdfdfa7470da162cfa9216db256f0be0caa4b0
hector <hector.morales@logicalsoft.co>
2019-01-15 19:11:23 -0500

    15012018 chatarreria front

---
+++ src/control_panel/forms/purchase_type_form.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class PurchaseTypeForm extends Component {
+
+  render() {
+    return (
+      <form>
+        <label for="nya">Nombres y Apellidos: (Solo lectura)</label>
+        <br />
+        <input type="text" name="nya" id="nya" readonly="true" />
+        <br /><br />
+        <label for="email">Email: (Campo Obligatorio)</label>
+        <br />
+        <input type="text" name="email" id="email" required />
+        <br /><br />
+        <label for="edad">Edad: (Ejemplo)</label>
+        <br/>
+        <input type="text" name="edad" id="edad" placeholder="Ejemplo: 23 años" />
+        <br /><br />
+        <input type="submit" value="Enviar" />
+      </form>
+    );
+  }
+}
+
+export default PurchaseTypeForm
