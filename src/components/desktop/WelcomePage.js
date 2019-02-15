81eae563535e9220d21c921a49c26f2308eda9f8
Hector Morales <hector.morales@logicalsoft.co>
2019-02-15 18:04:11 -0500

    15022019 cambios

--- src/components/desktop/WelcomePage.js
+++ src/components/desktop/WelcomePage.js
@@ -5,8 +5,7 @@ import logo_fondo from '../../images/logo_fondo.png?v1.0';
 class WelcomePage extends Component {	
   	render() {
   	  	return (
-  	  		<div className="LogoFondo">
-  	  			BIENVENIDO AL SOFTWARE ADMINISTRATIVO DE LA RECUPERADORA CAICEDO MARIN 		  
+  	  		<div className="LogoFondo">  	  					  
   	  			<img src={ logo_fondo } alt="" />
   	  		</div>		    		
 	    );
--- src/components/login/LogoLogin.js
+++ src/components/login/LogoLogin.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import LoginForm from './LoginForm';
 import logo_login from '../../images/logo_login.png?v1.0';
+import logo_login1 from '../../images/logo_login1.png?v1.0';
 
 class LogoLogin extends Component {
     
     render() {
         return (            
             <div className="LogoLogin">
-                <img src={ logo_login } alt="Acecolombia" />
+                <img src={ logo_login1 } alt="Acecolombia" />
             </div>                                         
         );
     }
