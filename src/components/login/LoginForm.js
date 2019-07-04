0a3470d3c48f8db7fc6756c392a754fe92fc59f0
hector <hector.morales@logicalsoft.co>
2019-07-04 17:13:45 -0500

    04072019 pantalla login

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -10,27 +10,34 @@ class LoginForm extends Component {
 
     constructor(props) {
         super(props);
+        this.inputEmpresa = React.createRef();
+        this.state = {empresa: ''};
+        this.state = {id_empresa: 0};
         this.state = {username: ''};
         this.state = {password: ''}; 
         this.handleLogin = this.handleLogin.bind(this);
     }
     handleLogin(val) {    
         //validacion de datos       
-        if(this.state.username === undefined || this.state.username === ''){
+        if(this.state.empresa === undefined || this.state.empresa === ''){
+            alertify.error('Digite la empresa!'); 
+            return;
+        }
+        else if(this.state.username === undefined || this.state.username === ''){
             alertify.error('Digite el nombre de usuario!'); 
             return;
         }   
-        else if(this.state.username === undefined || this.state.password === ''){
+        else if(this.state.password === undefined || this.state.password === ''){
             alertify.error('Digite el password!'); 
             return;
-        }
+        }         
         //consulta si el usuario existe con los datos ingresados
-        axios.get('http://localhost:5000/login/'+this.state.username+'/'+this.state.password, {withCredentials: true})
+        axios.get('http://localhost:5000/login/'+this.state.id_empresa+'/'+this.state.username+'/'+this.state.password, {withCredentials: true})
         .then(res => {
             var response = res.data; 
                       
             if(response.msg === 'notExist'){
-                alertify.error('Datos de ingreso incorrectos!'); 
+                alertify.error('Datos de ingreso incorrectos!');                 
             }
             else if(response.msg === 'error'){
                 alertify.alert('Error!', 'Ha ocurrido un error accesando a la base de datos!<br />Codigo de Error: '+response.detail);
@@ -40,10 +47,37 @@ class LoginForm extends Component {
             }          
         }) 
         .catch( err => {            
-            alertify.alert('Error!', 'No se ha logrado la conexion con el servidor!<br />'+err);
+            alertify.alert('Error!', 'No se ha logrado la conexion con el servidor!<br />'+err);            
         })
     }
-    handleIdChange(e) { 
+    validacionEmpresa(val) { 
+        //consulta si el usuario existe con los datos ingresados
+        axios.get('http://localhost:5000/validaEmpresa/'+this.state.empresa, {withCredentials: true})
+        .then(res => {
+            var response = res.data; 
+                      
+            if(response.msg === 'notExist'){
+                alertify.error('La empresa no existe!'); 
+                this.inputEmpresa.current.focus();
+            }
+            else if(response.msg === 'error'){
+                alertify.alert('Error!', 'Ha ocurrido un error accesando a la base de datos!<br />Codigo de Error: '+response.detail);
+                this.inputEmpresa.current.focus();
+            }
+            else{
+                this.setState({id_empresa: response[0].id}); //CARGAR EL ID EMPRESA                
+                alertify.success(response[0].razon_social);                
+            }          
+        }) 
+        .catch( err => {            
+            alertify.alert('Error!', 'No se ha logrado la conexion con el servidor!<br />'+err);
+            this.inputEmpresa.current.focus();
+        })
+    }
+    handleNitChange(e) { 
+        this.setState({empresa: e.target.value}); 
+    }
+    handleUserChange(e) { 
         this.setState({username: e.target.value}); 
     }
     handlePasswordChange(e) { 
@@ -54,10 +88,18 @@ class LoginForm extends Component {
             <form className="DivLogin">
                 <div className="ContentField">
                     <div className="FieldImage">
+                        <img alt="Empresa" src={ usuario_login } />
+                    </div>
+                    <div className="FieldDiv">
+                        <input type="text" className="mytext" name="empresa" id="empresa" placeholder="NIT Empresa" required onBlur={this.validacionEmpresa.bind(this)} onChange={this.handleNitChange.bind(this)}  ref={this.inputEmpresa}/>
+                    </div>
+                </div>
+                <div className="ContentField">
+                    <div className="FieldImage">
                         <img alt="Usuario" src={ usuario_login } />
                     </div>
                     <div className="FieldDiv">
-                        <input type="text" className="mytext" name="usuario" id="usuario" placeholder="Usuario" required onChange={this.handleIdChange.bind(this)} />
+                        <input type="text" className="mytext" name="usuario" id="usuario" placeholder="Usuario" required onChange={this.handleUserChange.bind(this)}/>
                     </div>
                 </div>
                 <div className="ContentField" styles= {{marginTop: '20px'}}>
@@ -69,7 +111,7 @@ class LoginForm extends Component {
                     </div>
                 </div>
                 <div className="ContentField" styles={{padding:'5px 15px 10px', margin:'0px', textAlign:'right'}}>
-                    <div style={{color:'#FFF'}}>
+                    <div style={{color:'#FFF',textAlign:'right',paddingRight:'10px'}}>
                         <input id="recordarme" name="recordarme" type="checkbox" value="true" style={{height:'auto'}} />Recordar mi Usuario
                     </div>
                 </div>
