84941887f51fbdb0b5fb0f13dcdfc25ef84ebe38
Hector Morales <hector.morales@logicalsoft.co>
2019-11-12 15:14:46 -0500

    12112019 permisos

--- src/components/reports/Reports.js
+++ src/components/reports/Reports.js
@@ -36,7 +36,9 @@ class Reports extends Component {
                         optionMenu={[
                                         {
                                             label : 'Informe de Compras',
-                                            table : 'purchases',                                                                                        
+                                            table : 'purchases',  
+                                            colsHeaders : ['Documento','Nombre','Email','Direccion','Telefono'],
+                                            colsData : ['documento','nombre','email','direccion','telefono'],                                                                                      
                                             dateFilter : 'true', 
                                             btnExcel : 'true',
                                             btnPDF : 'true'                                                                                      
@@ -54,84 +56,13 @@ class Reports extends Component {
                                             label : 'Informe de Ventas',
                                             table : 'sales',                                            
                                             dateFilter : 'true',
-                                            btnExcel : 'true',
-                                            btnPDF : 'true'                                                                                     
-                                        },                                        
-                                    ]}                        
-                    />
-                    <ReportOption 
-                        tab="ReportOption2" 
-                        title='Recicladores' 
-                        funcionClick = {this.actualizarReportContainer}                         
-                        position="2"
-                        optionWidth='250px'
-                        optionMenu={[
-                                        {
-                                            label : 'Informe de Recicladores',
-                                            table : 'reciclators',                                            
-                                            dateFilter : 'false',
-                                            btnExcel : 'true',
-                                            btnPDF : 'true'                                                                                   
-                                        },                                        
-                                    ]}                        
-                    /> 
-                    <ReportOption 
-                        tab="ReportOption2" 
-                        title='Clientes' 
-                        funcionClick = {this.actualizarReportContainer}                      
-                        position="3"
-                        optionWidth='230px'
-                        optionMenu={[
-                                        {
-                                            label : 'Informe de Clientes',
-                                            table : 'customers',                                            
-                                            dateFilter : 'false', 
-                                            btnExcel : 'true',
-                                            btnPDF : 'true'                                                                                    
-                                        },                                        
-                                    ]}                        
-                    />
-                    <ReportOption 
-                        tab="ReportOption2" 
-                        title='Usuarios' 
-                        funcionClick = {this.actualizarReportContainer}                        
-                        position="4"
-                        optionWidth='230px'
-                        optionMenu={[
-                                        {
-                                            label  : 'Informe de Usuarios',
-                                            table  : 'users',   
                                             colsHeaders : ['Documento','Nombre','Email','Direccion','Telefono'],
-                                            colsData : ['documento','nombre','email','direccion','telefono'],                                          
-                                            dateFilter : 'false',
+                                            colsData : ['documento','nombre','email','direccion','telefono'],
                                             btnExcel : 'true',
-                                            btnPDF : 'true'                                                                                      
+                                            btnPDF : 'true'                                                                                     
                                         },                                        
                                     ]}                        
-                    /> 
-                    <ReportOption 
-                        tab="ReportOption2" 
-                        title='Parametrizaciones' 
-                        funcionClick = {this.actualizarReportContainer}                       
-                        position="5"
-                        optionWidth='320px'
-                        optionMenu={[
-                                        {
-                                            label : 'Informe de Tipos de Documento',
-                                            table : 'document_types',                                            
-                                            dateFilter : 'false',
-                                            btnExcel : 'false',
-                                            btnPDF : 'true'                                                                                     
-                                        },
-                                        {
-                                            label : 'Informe de Tipos de Compra',
-                                            table : 'purchase_types',                                            
-                                            dateFilter : 'false',   
-                                            btnExcel : 'true',
-                                            btnPDF : 'false'                                                                                    
-                                        },                                                                                
-                                    ]}                        
-                    />             
+                    />          
                 </div>	
                 <div id="reportContainer" className="reportContainer" data-role="reportTbar" os="windows" >
                     <ReportContainer optionMenu={this.state.optionMenu} />
