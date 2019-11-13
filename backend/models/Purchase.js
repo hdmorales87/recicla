1831984ae52a8b4155b32b80cf0a761b56e7d9d2
hector morales <hector.morales@logicalsoft.co>
2019-11-12 22:18:58 -0500

    12112019 reports

--- backend/models/Purchase.js
+++ backend/models/Purchase.js
@@ -57,7 +57,29 @@ PurchaseModel.getPurchasesReport = function(userData, callback) {
     if (connection) {
         var searchWord   = userData.searchWord;
        // var showRecords  = userData.showRecords; 
-        //var offsetRecord = userData.offsetRecord;       
+        //var offsetRecord = userData.offsetRecord;
+        var sql = `SELECT 
+                        P.id,
+                        (P.peso * PT.precio_compra) AS valor_compra,
+                        DATE_FORMAT(P.fecha_compra,"%Y-%m-%d") AS fecha_compra,
+                        PT.id AS id_tipo_producto,
+                        PT.nombre AS tipo_producto,
+                        R.id AS id_reciclador,
+                        R.nombre AS reciclador,
+                        P.peso,
+                        P.id_empresa 
+                   FROM purchases AS P  
+                   INNER JOIN product_types AS PT ON (PT.id = P.id_tipo_producto) 
+                   INNER JOIN reciclators AS R ON (R.id = P.id_reciclador) 
+                   WHERE 
+                        P.fecha_compra BETWEEN \'`+filtroFecha1+`\' AND \'`+filtroFecha2+`\'
+                        AND P.id_empresa = `+userData.id_empresa+`
+                        AND (
+                            PT.nombre LIKE \'%`+searchWord+`%\' 
+                            OR R.nombre LIKE \'%`+searchWord+`%\'                                                 
+                            OR P.peso LIKE \'%`+searchWord+`%\')                         
+                   ORDER BY P.id LIMIT `+offsetRecord+','+showRecords;
+                          
         connection.query('SELECT * FROM purchases WHERE '
                         +' documento LIKE \'%'+searchWord+'%\' '
                         +' OR nombre LIKE \'%'+searchWord+'%\' '                                                
